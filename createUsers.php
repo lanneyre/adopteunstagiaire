648c008a73b3146447ac0c7142cb9095a737e9d2
Remi LANNEY <r.lanney@ics-nice.com>
2020-04-08 12:16:51 +0200

    login/mdp sécurisé + update bdd

--- createUsers.php
+++ createUsers.php
@@ -1,16 +1,16 @@
 <?php 
     require_once("include/bdd.php");
 
-    $sqlAllApprenants = "SELECT * FROM `stagiaire` WHERE `stagiaire_formation_id` = 1;";
+    $sqlAllApprenants = "SELECT * FROM `utilisateur`;";
         // que j'envoie au serveur
         $requeteAllApprenants = $db->query($sqlAllApprenants);
         // avant de récupérer les résultats
         $allApprenants = $requeteAllApprenants->fetchAll(PDO::FETCH_OBJ);
 
-    $sqlUsers = "INSERT INTO `utilisateur` (`utilisateur_id`, `utilisateur_mail`, `utilisateur_mdp`, `utilisateur_presentation`, `utilisateur_tel`) VALUES (NULL, :utilisateur_mail, :utilisateur_mdp, \"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam eius deleniti sit obcaecati, porro doloremque voluptatum a qui saepe consequatur! Optio rem quis unde laudantium laborum enim dolorum corporis architecto.\", \"06 21 34 59 78\");";
-    $reqUsers = $db->prepare($sqlUsers);
+    // $sqlUsers = "INSERT INTO `utilisateur` (`utilisateur_id`, `utilisateur_mail`, `utilisateur_mdp`, `utilisateur_presentation`, `utilisateur_tel`) VALUES (NULL, :utilisateur_mail, :utilisateur_mdp, \"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam eius deleniti sit obcaecati, porro doloremque voluptatum a qui saepe consequatur! Optio rem quis unde laudantium laborum enim dolorum corporis architecto.\", \"06 21 34 59 78\");";
+    // $reqUsers = $db->prepare($sqlUsers);
 
-    $SQLuptStagiaire = "UPDATE `stagiaire` SET `stagiaire_utilisateur_id` = :stagiaire_utilisateur_id WHERE `stagiaire`.`stagiaire_id` = :stagiaire_id ";
+    $SQLuptStagiaire = "UPDATE `utilisateur` SET `utilisateur_mdp` = :utilisateur_mdp WHERE `utilisateur`.`utilisateur_id` = :utilisateur_id ";
     $requpdStagiaire = $db->prepare($SQLuptStagiaire);
 
     foreach ($allApprenants as $apprenant) {
@@ -20,8 +20,12 @@
     	// $reqUsers->bindValue(":utilisateur_mail", $utilisateur_mail);
     	// $reqUsers->bindValue(":utilisateur_mdp", $utilisateur_mdp);
     	// $reqUsers->execute();
-    	$stagiaire_utilisateur_id = $apprenant->stagiaire_id + 3;
-    	$requpdStagiaire->bindValue(":stagiaire_utilisateur_id",$stagiaire_utilisateur_id);
-    	$requpdStagiaire->bindValue(":stagiaire_id", $apprenant->stagiaire_id);
-    	$requpdStagiaire->execute();
+    	// $stagiaire_utilisateur_id = $apprenant->stagiaire_id + 3;
+    	// $requpdStagiaire->bindValue(":stagiaire_utilisateur_id",$stagiaire_utilisateur_id);
+    	// $requpdStagiaire->bindValue(":stagiaire_id", $apprenant->stagiaire_id);
+    	// $requpdStagiaire->execute();
+
+        $requpdStagiaire->bindValue(":utilisateur_mdp",password_hash($apprenant->utilisateur_mdp, PASSWORD_BCRYPT));
+        $requpdStagiaire->bindValue(":utilisateur_id", $apprenant->utilisateur_id);
+        $requpdStagiaire->execute();
     }
--- login.php
+++ login.php
@@ -1,26 +1,62 @@
 <?php 
-
+	// je teste si les champs existet et si aucun n'est vide
 	if(!empty($_POST['utilisateur_mail']) && !empty($_POST['utilisateur_mdp'])){
+		// je teste si le mail est bien formé (pas s'il existe)
 		if(filter_var($_POST['utilisateur_mail'], FILTER_VALIDATE_EMAIL)){
 			// echo "ok";
+			// j'intègre ma connexion à la bdd
 			require_once("include/bdd.php");
+			// si les mdp ne sont pas cryptés
+			/*
 			try{
 				// Si vous voulez permettre l'injection SQL
-				$requeteSQL = "SELECT `utilisateur_mail` FROM `utilisateur` WHERE `utilisateur_mail` = '".$_POST['utilisateur_mail']."' AND `utilisateur_mdp` = '".$_POST['utilisateur_mdp']."';";
-				$reqprepare = $db->query($requeteSQL);
+				// $requeteSQL = "SELECT `utilisateur_mail` FROM `utilisateur` WHERE `utilisateur_mail` = '".$_POST['utilisateur_mail']."' AND `utilisateur_mdp` = '".$_POST['utilisateur_mdp']."';";
+				// $reqprepare = $db->query($requeteSQL);
 
 
 				// Et si vous voulez vous en protéger :
-				// $requeteSQL = "SELECT `utilisateur_mail` FROM `utilisateur` WHERE `utilisateur_mail` = :utilisateur_mail AND `utilisateur_mdp` = :utilisateur_mdp;";
-				// $reqprepare = $db->prepare($requeteSQL);
-				// $reqprepare->bindValue(":utilisateur_mail", $_POST['utilisateur_mail']);
-				// $reqprepare->bindValue(":utilisateur_mdp", $_POST['utilisateur_mdp']);
-				// $reqprepare->execute();
+				$requeteSQL = "SELECT `utilisateur_mail` FROM `utilisateur` WHERE `utilisateur_mail` = :utilisateur_mail AND `utilisateur_mdp` = :utilisateur_mdp;";
+				$reqprepare = $db->prepare($requeteSQL);
+				$reqprepare->bindValue(":utilisateur_mail", $_POST['utilisateur_mail']);
+				$reqprepare->bindValue(":utilisateur_mdp", $_POST['utilisateur_mdp']);
+				$reqprepare->execute();
 
 				if($reqprepare->rowCount() == 0){
 					echo "Les identifiants sont érronés";
 				} else {
 					echo "vous êtes connectés";
+				}
+			}catch(PDOException $exception){
+				echo $exception->getMessage();
+			}*/
+
+			// si les mdp sont cryptés
+			try{
+
+				// Et si vous voulez vous en protéger :
+				// Je récupère tous les utilisateurs en fonction du mail.
+				// Le mail étant définit comme unique dans la bdd : seul 2 cas sont possible : soit il y a 1 utilisateur soit il n'y en a pas
+				$requeteSQL = "SELECT `utilisateur_mail`, `utilisateur_mdp` FROM `utilisateur` WHERE `utilisateur_mail` = :utilisateur_mail";
+
+				// Afin d'éviter les injections SQL je prépare ma requête
+				$reqprepare = $db->prepare($requeteSQL);
+				// j'intègre le mail à ma requête
+				$reqprepare->bindValue(":utilisateur_mail", $_POST['utilisateur_mail']);
+				// j'exécute ma requete
+				$reqprepare->execute();
+
+				// Si ma requete ne renvoit aucune ligne : cela veut dire que l'email n'est pas dans la bdd
+				if($reqprepare->rowCount() == 0){
+					echo "Le mail n'est pas enregistré dans la bdd";
+				} else {
+					// je récupère les infos nottamment le mot de passe que je stocke dans la variable
+					$utilisateur = $reqprepare->fetch(PDO::FETCH_OBJ);
+					// ensuite je test si les mdp correspondent entre eux
+					if(password_verify($_POST['utilisateur_mdp'], $utilisateur->utilisateur_mdp)){
+						echo "vous êtes connectés";	
+					} else {
+						echo "Le mdp est faux";
+					}			
 				}
 			}catch(PDOException $exception){
 				echo $exception->getMessage();
